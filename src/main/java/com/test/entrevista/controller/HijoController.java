3c5bdeeb98802153cfe042712fdfdeaf252079fd
TomassCast99 <86583636+TomassCast99@users.noreply.github.com>
2023-07-27 18:06:49 -0300

    Le agrego los metodos de controller a Hijo y a Padre

--- src/main/java/com/test/entrevista/controller/HijoController.java
+++ src/main/java/com/test/entrevista/controller/HijoController.java
@@ -1,23 +1,55 @@
 package com.test.entrevista.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.entrevista.model.Hijo;
 import com.test.entrevista.service.IHijoService;
-import com.test.entrevista.service.IPersonaService;
 
 @RestController
 @RequestMapping("/hijo")
-public abstract class HijoController extends PersonaController<Hijo, Integer> {
+public class HijoController {
+	
+	 public IHijoService hijoService;
 
-	@Autowired
-	private IHijoService hijoService;
+	    @GetMapping
+		public ResponseEntity<List<Hijo>> listarT() {
+			List<Hijo> lista = hijoService.listarTodo();
+	        return new ResponseEntity<>(lista, HttpStatus.OK);
+	    }
 
-	@Override
-	public IPersonaService<Hijo, Integer> getService() {
-		return hijoService;
-	}
+	    @GetMapping("/{id}")
+	    public ResponseEntity<Hijo> obtenerTPorId(@PathVariable Integer id) {
+	        Hijo entidad = hijoService.obtenerPorId(id);
+	        return new ResponseEntity<>(entidad, HttpStatus.OK);
+	    }
+
+	    @PostMapping
+	    public ResponseEntity<Hijo> guardarT(@RequestBody Hijo t) {
+	        Hijo entidad = hijoService.guardar(t);
+	        return new ResponseEntity<>(entidad, HttpStatus.CREATED);
+	    }
+
+	    @PutMapping
+	    public ResponseEntity<Hijo> modificarT(@RequestBody Hijo t) {
+	        Hijo entidad = hijoService.modificar(t);
+	        return new ResponseEntity<>(entidad, HttpStatus.OK);
+	    }
+
+	    @DeleteMapping("/{id}")
+	    public ResponseEntity<Void> eliminarTPorId(@PathVariable Integer id) {
+	        hijoService.eliminarPorId(id);
+	        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    }
 
 }
